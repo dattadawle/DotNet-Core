22316d5c9e41588b97371a2737e88a51d6a09336
Dawle Datta Lingram <dattadawle72@gmail.com>
2025-01-30 14:27:49 +0530

    core1

---
+++ MVC _Core_Demos/MVC _Core_Demos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC__Core_Demos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
