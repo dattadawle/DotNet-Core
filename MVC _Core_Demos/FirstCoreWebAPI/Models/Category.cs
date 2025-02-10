9e2d414349faa0cfb4878e415cfd5a06b1e813d8
Dawle Datta Lingram <dattadawle71@gmail.com>
2025-02-10 23:00:02 +0530

    api

---
+++ MVC _Core_Demos/FirstCoreWebAPI/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace FirstCoreWebAPI.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
